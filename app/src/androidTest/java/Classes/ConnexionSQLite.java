5a54d00306ff9415110051fdf9e51ad2fa5e1b97
John-Eddy <rahmanieddy@gmail.com>
2017-11-24 16:27:36 +0100

    Création classe ConnexionSQLite

---
+++ app/src/androidTest/java/Classes/ConnexionSQLite.java
@@ -0,0 +1,12 @@
+package Classes;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Utilisateur on 24/11/2017.
+ */
+
+//public class ConnexionSQLite extends SQLiteOpenHelper {
+//}
